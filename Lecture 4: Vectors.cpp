a685be69ba4819db50d00ea1de08324cae02443f
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2024-09-02 20:33:16 -0700

    Update Lecture 4: Vectors.cpp
    changed

--- Lecture 4: Vectors.cpp
+++ Lecture 4: Vectors.cpp
@@ -8,58 +8,29 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main()
 {
-    std::cout<<"Hello World";
+    vector <int> first;
+    vector <int> second(4,300);
     
-    vector<int> v1;
+    vector<int> third(second.begin(), second.end());
     
-    //vector <AClass> record(20); // vecotr for a class called AClass with 20 elements
+    vector<int> fourth(third);
     
-    //note: <int>notation for a template class 
-    //vectors are a part of the STL (standard template library)
+    cout << "\nThe contents of the vector are: ";
     
-    //unsigned int only stores positive integers
+    for (vector<int>::iterator counter = fourth.begin();
     
-    vector<int> vectorArray2;
-    cout << endl;
-    cout << vectorArray2.size(); //returns a number of elements in the vectorArray2
-    cout << endl;
-    cout << vectorArray2.capacity(); //returns a number of capacity in the vectorArray2
-    cout << endl;
+    counter != fourth.end();
     
-    cout << "myVector(10)" << endl;
+    ++counter)
     
-    int lenght = 10;
-    
-    vector<int> myVector(lenght);
-    
-    cout << endl;
-    cout << myVector.size() << endl;;
-    cout << myVector.capacity() << endl;
-    cout << endl;
-    
-    myVector.push_back(9);
-    myVector.push_back(8);
-    myVector.push_back(7);
-    
-    cout << endl;
-    cout << myVector.size() << endl;;
-    cout << myVector.capacity() << endl;
-    cout << endl;
-    
-    myVector.begin();
-    myVector.end();
-    
-    for (int i = 0; i < lenght; i++)
     {
-        myVector[i] = i;
-        
-        cout << "myVector: " << myVector[i] << endl;
+        cout << *counter << " ";
     }
+    cout << endl;
 
     return 0;
 }
